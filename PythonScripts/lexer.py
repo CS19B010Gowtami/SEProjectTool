1c2193dc664a5ae99c061e519aafd5e15043e6aa
SahithiRani <sahithirani2002@gmail.com>
2022-03-22 14:39:56 +0530

    update lexer

--- PythonScripts/lexer.py
+++ PythonScripts/lexer.py
@@ -1,166 +1,158 @@
-from sly import lexer
+from sly import Lexer
 
 class MyLexer(Lexer):
+
     # these are set of tokens that are to be exported to the parser
-   tokens = {
-            REALNUM,
-			INTNUM,
-			STRING,
-			CHARACTER,
-		IDENTIFIER,
-			EQUAL,
-			ADDEQ,
-			SUBEQ,
-			MULEQ,
-			DIVEQ,
-			MODEQ,
-			GTEQ,
-			LTEQ,
-			NOTEQ,
-			ANDEQ,
-			OREQ,
-			EXLUSIVE_EQ,
-			SUBOP,
-			MULOP,
-			DIVOP,
-			MODOP,
-			GTOP,
-			LTOP,
-			ANDOP,
-			OROP,
-			EXLUSIVE_OP,
-			SEPARATORS,
-			SEMICOLON,
-			LCB,
-			RCB,
-			LFB,
-			RFB,
-			LSB,
-			RSB
+    tokens = {
+            "REALNUM",
+			"INTNUM",
+			"STRING",
+			"CHARACTER",
+		    "IDENTIFIER",
+			"EQUAL",
+			"ADDEQ",
+			"SUBEQ",
+			"MULEQ",
+			"DIVEQ",
+			"MODEQ",
+			"GTEQ",
+			"LTEQ",
+			"NOTEQ",
+			"ANDEQ",
+			"OREQ",
+			"EXLUSIVE_EQ",
+			"SUBOP",
+			"MULOP",
+			"DIVOP",
+			"MODOP",
+			"GTOP",
+			"LTOP",
+			"ANDOP",
+			"OROP",
+			"EXLUSIVE_OP",
+			"SEPARATORS",
+			"SEMICOLON",
+			"LCB",
+			"RCB",
+			"LFB",
+			"RFB",
+			"LSB",
+			"RSB"
 		}
 
-@_(r'\d+')
-def INTNUM(self, t):
-    t.value = int(t.value)
-    return t
+    # Identifiers and keywords
+    ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
-@_(r'\d+.\d+')
-def REALNUM(self, t):
-    t.value = float(t.value)
-    return t
+    ID['ADD']  =  ADD
+    ID['CONSTRAINT']  = CONSTRAINT
+    ID['ALTER']  =  ALTER
+    ID['COLUMN']  =  COLUMN
+    ID['TABLE']  = TABLE
+    ID['AS']  =  AS
+    ID['ASC']  = ASC
+    ID['BACKUP']  =  BACKUP
+    ID['DATABASE']  =  DATABASE
+    ID['CASE']  =  CASE
+    ID['CHECK']  =  CHECK
+    ID['CREATE']  =  CREATE
+    ID['INDEX']  =  INDEX
+    ID['REPLACE'] = REPLACE
+    ID['VIEW'] = VIEW
+    ID['PROCEDURE'] = PROCEDURE
+    ID['UNIQUE'] = UNIQUE
+    ID['DEFAULT'] = DEFAULT
+    ID['DELETE'] = DELETE
+    ID['DESC'] = DESC
+    ID['DISTINCT'] = DISTINCT
+    ID['DROP'] = DROP
+    ID['USE'] = USE
+    ID['SHOW'] = SHOW
+    ID['EXEC'] = EXEC
+    ID['FOREIGN'] = FOREIGN
+    ID['KEY'] = KEY
+    ID['FROM'] = FROM
+    ID['FULL'] = FULL
+    ID['OUTER'] = OUTER
+    ID['JOIN'] = JOIN
+    ID['GROUP'] = GROUP
+    ID['BY'] = BY
+    ID['HAVING'] = HAVING
+    ID['INNER'] = INNER
+    ID['INSERT'] = INSERT
+    ID['INTO'] = INTO
+    ID['SELECT'] = SELECT
+    ID['NULL'] = NULL
+    ID['IS'] = IS
+    ID['LEFT'] = LEFT
+    ID['LIMIT'] = LIMIT
+    ID['ORDER'] = ORDER
+    ID['PRIMARY'] = PRIMARY
+    ID['RIGHT'] = RIGHT
+    ID['ROWNUM'] = ROWNUM
+    ID['TOP'] = TOP
+    ID['SET'] = SET
+    ID['TRUNCATE'] = TRUNCATE
+    ID['UNION'] = UNION
+    ID['UPDATE'] = UPDATE
+    ID['VALUES'] = VALUES
+    ID['WHERE'] = WHERE
+    ID['VARCHAR'] = VARCHAR
+    ID['ALL'] = ALL
+    ID['AND'] = AND
+    ID['ANY'] = ANY
+    ID['BETWEEN'] = BETWEEN
+    ID['EXISTS'] = EXISTS
+    ID['IN'] = IN
+    ID['LIKE'] = LIKE
+    ID['NOT'] = NOT
+    ID['OR'] = OR
+    ID['SOME'] = SOME
 
- # Identifiers and keywords
-ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
-ID['ADD']  =  ADD
-ID['CONSTRAINT']  = CONSTRAINT
-ID['ALTER']  =  ALTER
-ID['COLUMN']  =  COLUMN
-ID['TABLE']  = TABLE
-ID['AS']  =  AS
-ID['ASC']  = ASC
-ID['BACKUP']  =  BACKUP
-ID['DATABASE']  =  DATABASE
-ID['CASE']  =  CASE
-ID['CHECK']  =  CHECK
-ID['CREATE']  =  CREATE
-ID['INDEX']  =  INDEX
-ID['REPLACE'] = REPLACE
-ID['VIEW'] = VIEW
-ID['PROCEDURE'] = PROCEDURE
-ID['UNIQUE'] = UNIQUE
-ID['DEFAULT'] = DEFAULT
-ID['DELETE'] = DELETE
-ID['DESC'] = DESC
-ID['DISTINCT'] = DISTINCT
-ID['DROP'] = DROP
-ID['USE'] = USE
-ID['SHOW'] = SHOW
-ID['EXEC'] = EXEC
-ID['FOREIGN'] = FOREIGN
-ID['KEY'] = KEY
-ID['FROM'] = FROM
-ID['FULL'] = FULL
-ID['OUTER'] = OUTER
-ID['JOIN'] = JOIN
-ID['GROUP'] = GROUP
-ID['BY'] = BY
-ID['HAVING'] = HAVING
-ID['INNER'] = INNER
-ID['INSERT'] = INSERT
-ID['INTO'] = INTO
-ID['SELECT'] = SELECT
-ID['NULL'] = NULL
-ID['IS'] = IS
-ID['LEFT'] = LEFT
-ID['LIMIT'] = LIMIT
-ID['ORDER'] = ORDER
-ID['PRIMARY'] = PRIMARY
-ID['RIGHT'] = RIGHT
-ID['ROWNUM'] = ROWNUM
-ID['TOP'] = TOP
-ID['SET'] = SET
-ID['TRUNCATE'] = TRUNCATE
-ID['UNION'] = UNION
-ID['UPDATE'] = UPDATE
-ID['VALUES'] = VALUES
-ID['WHERE'] = WHERE
-ID['VARCHAR'] = VARCHAR
-ID['ALL'] = ALL
-ID['AND'] = AND
-ID['ANY'] = ANY
-ID['BETWEEN'] = BETWEEN
-ID['EXISTS'] = EXISTS
-ID['IN'] = IN
-ID['LIKE'] = LIKE
-ID['NOT'] = NOT
-ID['OR'] = OR
-ID['SOME'] = SOME
+    EQUAL = r'\='
 
-EQUAL = r'\='
+    # Arithmetic Assignment Operators
+    ADDEQ = r'\+\='
+    SUBEQ = r'\-\='
+    MULEQ = r'\*\='
+    DIVEQ = r'\/\='
+    MODEQ = r'\%\='
 
-# Arithmetic Assignment Operators
-ADDEQ = r'\+\='
-SUBEQ = r'\-\='
-MULEQ = r'\*\='
-DIVEQ = r'\/\='
-MODEQ = r'\%\='
+    # Comparison Operators
+    GTEQ = r'\>\='
+    LTEQ = r'\<\='
+    NOTEQ = r'\<\>'
 
-# Comparison Operators
-GTEQ = r'\>\='
-LTEQ = r'\<\='
-NOTEQ = r'\<\>'
+    # Bitwise Assignment Operators
+    ANDEQ = r'\&\='
+    OREQ = r'\|\='
+    EXLUSIVE_EQ = r'\^\='
 
-# Bitwise Assignment Operators
-ANDEQ = r'\&\='
-OREQ = r'\|\='
-EXLUSIVE_EQ = r'\^\='
+    # Arithmetic Operators
+    ADDOP = r'\+'
+    SUBOP = r'\-'
+    MULOP = r'\*'
+    DIVOP = r'\/'
+    MODOP = r'\%'
 
-# Arithmetic Operators
-ADDOP = r'\+'
-SUBOP = r'\-'
-MULOP = r'\*'
-DIVOP = r'\/'
-MODOP = r'\%'
+    # Comparison Operators
+    GTOP = r'\>'
+    LTOP = r'\<'
 
-# Comparison Operators
-GTOP = r'\>'
-LTOP = r'\<'
+    # Bitwise Operators
+    ANDOP = r'\&'
+    OROP = r'\|'
+    EXLUSIVE_OP = r'\^'
 
-# Bitwise Operators
-ANDOP = r'\&'
-OROP = r'\|'
-EXLUSIVE_OP = r'\^'
+    SEPARATORS = r'\,'
+    SEMICOLON = r'\;'
+    LCB = r'\('
+    RCB = r'\)'
+    LFB = r'\{'
+    RFB = r'\}'
+    LSB = r'\['
+    RSB = r'\]'
 
-SEPARATORS = r'\,'
-SEMICOLON = r'\;'
-LCB = r'\('
-RCB = r'\)'
-LFB = r'\{'
-RFB = r'\}'
-LSB = r'\['
-RSB = r'\]'
-
-ignore = '\t'
+    ignore = '\t'
 
 # Identifier refers to table name, attribute name etc
 # @_(r'[a-zA-Z]+')
@@ -186,11 +178,20 @@ ignore = '\t'
 # def NUMBER(self, t):
 #         t.value = int(t.value)
 #         return t
+    @_(r'\d+')
+    def INTNUM(self, t):
+     t.value = int(t.value)
+     return t
 
-@_(r'''("[^"\\]*(\\.[^"\\]*)*"|'[^'\\]*(\\.[^'\\]*)*')''')
-def STRING(self, t):
-    t.value = self.remove_quotes(t.value)
-    return t
+    @_(r'\d+.\d+')
+    def REALNUM(self, t):
+	 t.value = float(t.value)
+	 return t
+
+    @_(r'''("[^"\\]*(\\.[^"\\]*)*"|'[^'\\]*(\\.[^'\\]*)*')''')
+    def STRING(self, t):
+	 t.value = self.remove_quotes(t.value)
+	 return t
 
 @_(r'\d+.\d+')
 def REALNUM(self, t):
@@ -203,20 +204,20 @@ def INTNUM(self, t):
 
 ignore_comment = r'\#.*'
 
-    # Line number tracking
+	# Line number tracking
 @_(r'\n+')
 def ignore_newline(self, t):
-    self.lineno += t.value.count('\n')
+	self.lineno += t.value.count('\n')
 
 def error(self, t):
-    print('Line %d: Bad character %r' % (self.lineno, t.value[0]))
-    self.index += 1
+	print('Line %d: Bad character %r' % (self.lineno, t.value[0]))
+	self.index += 1
 
 if __name__ == '__main__':
-    data = '''
+	data = '''
 WHILE count <= 10
 HAVING CREATE AS 
 '''
-    lexer = MyLexer()
-    for tok in lexer.tokenize(data):
-        print(tok)
+	lexer = MyLexer()
+	for tok in lexer.tokenize(data):
+		print(tok)
