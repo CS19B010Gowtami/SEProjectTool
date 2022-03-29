dcf379fa13b8af5b4f82ea800d98f49cea0d5915
roopasree123 <cs19b008@iittp.ac.in>
2022-03-29 09:41:20 +0530

    delete update added

--- PythonScripts/parser.py
+++ PythonScripts/parser.py
@@ -10,189 +10,41 @@ class MyParser(Parser):
     # def select(self, p):
     #     return p.expr + p.term
 
-
-    # start - query_list
     @_('query_list')
     def start(self,p):
-        return
+    	return
 
-    # query_list - query query_list
     @_('query query_list')
     def query_list(self,p):
         return
 
-    @_('select_stmt SEMICOLON')
+    @_('select_stmt')
     def query(self,p):
         return
 
-    @_('update_stmt SEMICOLON')
+    @_('update_stmt')
     def query(self,p):
         return
 
-    @_('insert_stmt SEMICOLON')
+    @_('insert_stmt')
     def query(self,p):
         return
 
-    @_('delete_stmt SEMICOLON')
+    @_('delete_stmt')
     def query(self,p):
         return
-    
-    # --------------- INSERT STATEMENT ---------------
-    @_('INSERT INTO IDENTIFIER LCB insert_col_list RCB VALUES LCB val_list RCB')
-    def insert_stmt(self, p):
+
+    @_('')
+    def empty(self, p):
+        pass
+
+    @_('empty')
+    def query(self,p):
         return
 
-    @_('insert_col_list COMMA IDENTIFIER')
-    def insert_col_list(self, p):
-        return
-    
-    @_('IDENTIFIER')
-    def insert_col_list(self, p):
-        return
-
-    @_('val_list COMMA val')
-    def val_list(self, p):
-        return
-    
-    @_('val')
-    def val_list(self, p):
-        return
-    # --------------- INSERT STATEMENT ---------------
-
-    # --------------- SELECT STATEMENT ---------------
-    @_('SELECT is_distinct select_param FROM IDENTIFIER select_opt_where sort_order opt_limit')
+    @_('SELECT is_distinct col_list FROM table_list opt_where_cond SEMICOLON')
     def select_stmt(self, p):
-        return 
-
-    @_('DISTINCT')
-    def is_distinct(self, p):
-        return 1
-
-    @_('')
-    def is_distinct(self, p):
-        return 0
-
-    @_('select_col_list')
-    def select_param(self, p):
         return
-
-    @_('MULOP')
-    def select_param(self, p):
-        return
-
-    @_('MAX LCB IDENTIFIER RCB')
-    def select_param(self, p):
-        return
-    
-    @_('MIN LCB IDENTIFIER RCB')
-    def select_param(self, p):
-        return
-
-    @_('COUNT LCB IDENTIFIER RCB')
-    def select_param(self, p):
-        return
-
-    @_('COUNT LCB MULOP RCB')
-    def select_param(self, p):
-        return
-
-    @_('SUM LCB IDENTIFIER RCB')
-    def select_param(self, p):
-        return
-
-    @_('AVG LCB IDENTIFIER RCB')
-    def select_param(self, p):
-        return
-
-    @_('select_col_list COMMA IDENTIFIER opt_aliasing')
-    def select_col_list(self, p):
-        return
-    
-    @_('IDENTIFIER opt_aliasing')
-    def select_col_list(self, p):
-        return
-
-    @_('AS IDENTIFIER')
-    def opt_aliasing(self,p):
-        return
-
-    @_('WHERE condition_list')
-    def select_opt_where(self,p):
-        return
-
-    @_('WHERE IDENTIFIER IS NULL')
-    def select_opt_where(self, p):
-        return
-
-    @_('WHERE IDENTIFIER IS NOT NULL')
-    def select_opt_where(self, p):
-        return
-
-    @_('')
-    def select_opt_where(self,p):
-        return
-
-    @_('NOT condition_list')
-    def condition_list(self, p):
-        return
-    
-    @_('condition ANDOP condition_list')
-    def condition_list(self, p):
-        return
-
-    @_('condition OROP condition_list')
-    def condition_list(self, p):
-        return
-
-    @_('condition')
-    def condition_list(self, p):
-        return
-
-    @_('ORDER BY ASC')
-    def sort_order(self, p):
-        return 0
-
-    @_('ORDER BY DESC')
-    def sort_order(self, p):
-        return 1
-
-    @_('')
-    def sort_order(self, p):
-        return 0
-
-    @_('LIMIT INTNUM OFFSET INTMUM')
-    def opt_limit(self, p):
-        return
-
-    @_('LIMIT INTNUM')
-    def opt_limit(self, p):
-        return
-
-    @_('')
-    def opt_limit(self, p):
-        return
-    # --------------- SELECT STATEMENT ---------------
-
-    # --------------- DELETE STATEMENT ---------------
-    @_('DELETE FROM IDENTIFIER delete_opt_where')
-    def delete_stmt(self, p):
-        return
-
-    @_('WHERE condition_list')
-    def delete_opt_where(self,p):
-        return
-
-    @_('')
-    def delete_opt_where(self,p):
-        return
-
-    # Incomplete ig
-    # --------------- DELETE STATEMENT ---------------
-
-
-    # @_('SELECT is_distinct col_list FROM table_list opt_where_cond SEMICOLON')
-    # def select_stmt(self, p):
-    #     return
 
     # @_('SELECT is_distinct col_list FROM table_list nest_cond opt_where_cond  SEMICOLON')
     # def select_stmt(self, p):
@@ -203,12 +55,24 @@ class MyParser(Parser):
     # def select_stmt(self,p):
     #     return
 
+    @('DISTINCT','empty')
+    def is_distinct(self,p):
+        return
+
+    @_('empty')
+    def opt_where_clause(self,p):
+        return
+
     @_('ID COMMA ID', 'ID','*')
     def col_list(self,p):
         return
 
     @_('ID COMMA ID','ID')
     def tab_list(self, p):
+        return
+
+    @_('WHERE conds','empty')
+    def opt_where_cond(self,p):
         return
   
 #   NESTED CONDITION
@@ -224,13 +88,20 @@ class MyParser(Parser):
     # def select_stmt(self, p):
     #     return
 
-    # @_('conds OR cond','conds AND cond','NOT cond','cond')
+    @_('conds OR cond','conds AND cond','NOT cond','cond')
+    def conds(self, p):
+        return 
+        
+    # @_('conds AND cond')
+    # def conds(self, p):
+    #     return
+    # @_('NOT cond')
     # def conds(self, p):
     #     return 
-
     # @_('cond')
     # def conds(self, p):
-    #     return p.cond
+    #     return
+
     # @_('cond')
     # def cond(self, p):
     #     return p.cond
@@ -254,20 +125,38 @@ class MyParser(Parser):
     # @_('ID NOTEQ NUMS')
     # def cond(self, p):
     #     return p.ID != p.NUMS
+    @_('UPDATE table_name SET col_assigns where_stmt SEMICOLON')
+    def update_stmt(self, p):
+        return 
+    
+    @_('col_assigns COMMA col_assign')
+    def col_assigns(self, p):
+        return
+    
+    @_('column_name EQUAL value')
+    def col_assign(self, p):
+        return p.column_name '=' p.value
+    
+    @_('ID')
+    def column_name(self, p):
+        return
+
+    @_('WHERE cond', 'empty')
+    def where_stmt(self, p):
+        return
+    
+    @_('INTNUM','REALNUM','STRING')
+    def value(self, p):
+        return 
 
     @_('ID')
     def item(self, p):
         return p.ID
 
-    @_('')
-    def empty(self, p):
-        pass
-
-    @_('empty')
-    def query(self,p):
+    @_('DELETE FROM table_name where_stmt SEMICOLON')
+    def delete_stmt(self, p):
         return
-
-
+        
 if __name__ == '__main__':
     lexer = MyLexer()
     parser = MyParser()
